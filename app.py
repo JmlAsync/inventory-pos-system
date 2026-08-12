b233c82f96c1000875952458614214f1d5b759d8
Jml <jmlsabado05@email.com>
2026-08-12 17:59:20 +0800

    Add Product model and initialize database

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask
+from models import db, Product
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///inventory.db'
+db.init_app(app)
 
 @app.route('/')
 def home():
     return "Inventory & POS System is running!"
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
